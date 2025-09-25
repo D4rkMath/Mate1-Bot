179d9c9c5dbb54c01f30c8d16fb17f13cf182347
D4rkLorD <135694320+D4rkMath@users.noreply.github.com>
2025-09-25 07:03:28 -0500

    Update tutoriales.js

--- tutoriales.js
+++ tutoriales.js
@@ -2,8 +2,8 @@ console.log("Tutoriales cargados");
 document.querySelector('.carousel-inner').innerHTML = `
   <div class="carousel-item">
     <div class="video-embed-container">
-      <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/5Boyptd7wY8" frameborder="0" allowfullscreen></iframe>
     </div>
-    <h4>Ejemplo de video</h4>
+    <h4>Tabular: FX-991LA CW</h4>
   </div>
 `;
